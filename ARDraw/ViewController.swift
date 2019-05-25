d29276cef7065314f05978cbc03482b742b4c15d
ByteSizeCode <you@yourdomain.com>
2019-05-25 13:09:18 -0700

    Initial Commit

---
+++ ARDraw/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ARDraw
+//
+//  Created by Isaac Raval on 5/25/19.
+//  Copyright © 2019 Isaac Raval. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var draw: UIButton!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.session.run(configuration) //Start ARSession
+        self.sceneView.delegate = self
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        
+        //Get position of camera
+        guard let pointOfView = sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let currentPositionOfCamera = orientation + location
+        
+        //Draw
+        DispatchQueue.main.async {
+            //While button is being held, draw
+            if self.draw.isHighlighted {
+                let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
+                sphereNode.position = currentPositionOfCamera
+                self.sceneView.scene.rootNode.addChildNode(sphereNode)
+                sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+            }
+            //If not pressing button, show a pointer
+            else {
+                let pointer = SCNNode(geometry: SCNSphere(radius: 0.01))
+                pointer.name = "pointer"
+                pointer.position = currentPositionOfCamera
+                self.sceneView.scene.rootNode.enumerateChildNodes({ (node, _) in
+                    if node.name == "pointer" {
+                        node.removeFromParentNode()
+                    }
+                })
+                self.sceneView.scene.rootNode.addChildNode(pointer)
+                pointer.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+            }
+        }
+    }
+}
+
+//Overload + operator to enable summing SCNVector3's
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# ARDraw
+
+A simple proof-of-concept AR app that enables drawing in the real world.
+<img src="/Screenshot/Screenshot.png" alt="drawing" width="350" height="500"/>
